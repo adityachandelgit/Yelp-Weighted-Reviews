4c899110d588680e2114d80f0c9b3188ff117236
Aditya Chandel <aditya.chandel@aggiemail.usu.edu>
2017-03-29 12:29:14 -0600

    Saved elite users id to text file

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import json
+from time import time
+
+
+def get_elite_users_id(input_path, output_path):
+    t = time()
+    print 'Getting elite users ID and saving them to txt...'
+    with open(input_path) as infile, open(output_path, "wb") as outfile:
+        for line in infile:
+            json_line = json.loads(line)
+            if json_line['elite'][0] != 'None':
+                outfile.write(json_line['user_id'] + '\n')
+    outfile.close()
+    print("done in %0.3fs." % (time() - t))
+
+
+if __name__ == "__main__":
+    get_elite_users_id('../data/input/yelp/yelp_academic_dataset_user.json', '../data/output/elite_users_id.json')
